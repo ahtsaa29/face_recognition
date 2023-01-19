d82e28b8d36ab8ec3b4f0199bca71fd3f19ee01a
Aastha Bajracharya <aasthabajracharya57@gmail.com>
2023-01-19 18:04:13 +0545

    done

---
+++ recog.py
@@ -0,0 +1,9 @@
+from identify_face import *
+from store_face import *
+
+
+face_store()
+face_identify()
+
+# for x in datasets:
+#     print(x)
--- store_face.py
+++ store_face.py
@@ -5,30 +5,34 @@ haar_file = 'haarcascade_frontalface_default.xml'
 face_cap = cv2.CascadeClassifier(haar_file)
 datasets = 'datasets'  
 video_cap = cv2.VideoCapture(0)
+
+def face_store():
 # These are sub data sets of folder for faces 
-sub_data = input('Enter Your Name:') 
+    sub_data = input('Enter Your Name:') 
 
-path = os.path.join(datasets, sub_data)
-if not os.path.isdir(path):
-    os.mkdir(path)
-  
-# defining the size of images 
-(width, height) = (130, 100)    
+    path = os.path.join(datasets, sub_data)
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    
+    # defining the size of images 
+    (width, height) = (130, 100)    
 
-  
-# The program loops until it has 30 images of the face.
-count = 0
-while count < 30: 
-    (_, cap_data) = video_cap.read()
-    col = cv2.cvtColor(cap_data, cv2.COLOR_BGR2GRAY)
-    faces = face_cap.detectMultiScale(col, 1.3, 4)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(cap_data, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = col[y:y + h, x:x + w]
-        face_resize = cv2.resize(face, (width, height))
-        cv2.imwrite('% s/% s.png' % (path, count), face_resize)
-    count += 1
-      
-    cv2.imshow('Reading your image', cap_data)
-    if cv2.waitKey(100) == ord("z"):
-        break
+    
+    # The program loops until it has 30 images of the face.
+    count = 0
+    while count < 30: 
+        (_, cap_data) = video_cap.read()
+        col = cv2.cvtColor(cap_data, cv2.COLOR_BGR2GRAY)
+        faces = face_cap.detectMultiScale(col, 1.3, 4)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(cap_data, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face = col[y:y + h, x:x + w]
+            face_resize = cv2.resize(face, (width, height))
+            cv2.imwrite('% s/% s.png' % (path, count), face_resize)
+        count += 1
+        
+        cv2.imshow('Reading your image', cap_data)
+        if cv2.waitKey(100) == ord("z"):
+            break
+
+face_store()
